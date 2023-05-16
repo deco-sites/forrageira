457c7f14f53c430b36c77936d48bc4534a5c9be3
Marcos Candeia <marrcooos@gmail.com>
2023-05-16 13:24:18 -0300

    Add polyfill
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -2,11 +2,20 @@ import { useState } from "preact/hooks";
 import { Button } from "../components/Button.tsx";
 
 try {
-  const resp = await fetch("/abcde");
-  console.log(resp);
+  const result = await fetch("/abcde");
+  console.log(result);
 } catch (err) {
-  console.log("err", err);
+  console.log("fetch finished", err);
 }
+// Lazy load a <dialog> polyfill.
+(async () => {
+  // @ts-expect-error HTMLDialogElement is not just a type!
+  if (IS_BROWSER && window.HTMLDialogElement === "undefined") {
+    await import(
+      "https://raw.githubusercontent.com/GoogleChrome/dialog-polyfill/5033aac1b74c44f36cde47be3d11f4756f3f8fda/dist/dialog-polyfill.esm.js"
+    );
+  }
+})();
 interface CounterProps {
   start: number;
 }
