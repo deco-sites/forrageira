65d6f7e0ed78fd562f8e72b59de6b9e87ef96ae2
Marcos Candeia <marrcooos@gmail.com>
2023-08-04 20:28:58 -0300

    Update do latest deco ver
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- packs/fashion.ts
+++
@@ -1,8 +0,0 @@
-import { DecoManifest } from "$live/mod.ts";
-import manifest from "deco-sites/fashion/live.gen.ts";
-
-export const name = "deco-sites/fashion";
-
-export default function Fashion(_props: unknown): DecoManifest {
-  return manifest as unknown as DecoManifest;
-}
