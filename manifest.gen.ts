1e50c646830c0f57756eeede22f7c1fc29662698
Marcos Candeia <marrcooos@gmail.com>
2024-03-05 14:03:42 -0300

    Add decohub
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -2,17 +2,19 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $$$$$$0 from "./sections/YTFeed.tsx";
-import * as $$$$$$1 from "./sections/LinkTree.tsx";
-import * as $$$$$$$$$$$0 from "./apps/site.ts";
+import * as $$$$$$$$$$$0 from "./apps/decohub.ts";
+import * as $$$$$$$$$$$1 from "./apps/site.ts";
+import * as $$$$$$0 from "./sections/LinkTree.tsx";
+import * as $$$$$$1 from "./sections/YTFeed.tsx";
 
 const manifest = {
   "sections": {
-    "deco-sites/forrageira/sections/LinkTree.tsx": $$$$$$1,
-    "deco-sites/forrageira/sections/YTFeed.tsx": $$$$$$0,
+    "deco-sites/forrageira/sections/LinkTree.tsx": $$$$$$0,
+    "deco-sites/forrageira/sections/YTFeed.tsx": $$$$$$1,
   },
   "apps": {
-    "deco-sites/forrageira/apps/site.ts": $$$$$$$$$$$0,
+    "deco-sites/forrageira/apps/decohub.ts": $$$$$$$$$$$0,
+    "deco-sites/forrageira/apps/site.ts": $$$$$$$$$$$1,
   },
   "name": "deco-sites/forrageira",
   "baseUrl": import.meta.url,
